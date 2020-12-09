ab2fa9e4537346b05708753ad86edcc30d813243
Andrew Phillipo <andrewphillipo@gmail.com>
2020-12-09 17:18:07 +0000

    part 2

--- day9/part1.js
+++ day9/part1.js
@@ -2,5 +2,35 @@ import * as fs from 'fs';
 
 const data = fs.readFileSync('./data.txt').toString();
 
-const input = data.split('\n');
+const input = data.split('\n').map((value) => parseInt(value, 10));
 
+console.log(input);
+
+const findNotSum = (inputNumbers, windowSize = 25) => {
+
+  // find the inital 25 numbers
+  const prev25Numbers = inputNumbers.slice(0, windowSize);
+
+  for (let i = 25, len = inputNumbers.length; i < len; i++) {
+    const number = inputNumbers[i];
+    let isValid = false;
+    for (let j = 0; j < prev25Numbers.length; j++) {
+      const candidateFactor = number - prev25Numbers[j];
+      if (prev25Numbers.includes(candidateFactor)) {
+        isValid = true;
+        break;
+      }
+    }
+    if (isValid) {
+      // remove the first item and add this new number
+      prev25Numbers.shift();
+      prev25Numbers.push(number);
+    } else {
+      // fail and don't carry on.
+      console.log('FAILS ON: ', number);
+      break;
+    }
+  }
+}
+
+findNotSum(input);
--- day9/part2.js
+++ day9/part2.js
@@ -2,5 +2,34 @@ import * as fs from 'fs';
 
 const data = fs.readFileSync('./data.txt').toString();
 
-const input = data.split('\n');
+const input = data.split('\n').map((value) => parseInt(value, 10));
 
+const previousPuzzleAnswer = 373803594;
+
+const findRangeThatSumsToTotal = (inputNumbers) => {
+  for (let windowSize = 3; windowSize < inputNumbers.length; windowSize++) {
+    for (let i = 0; i < inputNumbers.length; i++) {
+      let total = 0;
+      console.log('numbers');
+      let prevNumber = inputNumbers[i];
+      let smallest = Infinity;
+      let largest = 0;
+      for (let j = i; j <= i + windowSize; j++) {
+        const number = inputNumbers[j];
+        if (number < smallest) {
+          smallest = number;
+        }
+        if (number > largest) {
+          largest = number;
+        }
+        total += number;
+      }
+      if (total === previousPuzzleAnswer) {
+        console.log('found - total', total);
+        return { smallest, largest, total: smallest + largest, windowSize };
+      }
+    }
+  }
+}
+
+console.log(findRangeThatSumsToTotal(input));
